ca7cf185af4dd127b3479b44fb0c1171ef9e5aac
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-11-23 07:51:02 +0530

    adding test suite, update actions pipeline

--- test/valistclient.test.ts
+++ test/valistclient.test.ts
@@ -1,11 +1,11 @@
-import { ethers, providers } from "ethers";
+const { providers } = require("ethers");
 import ValistLicenseClient from "../src/index";
 
 
 describe('Valist License Client Initialization', () => {
   it('should initialize client without error', () => {
     const provider = new providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com");
-    const valistLicenseClient = new ValistLicenseClient(provider, 8001);
+    const valistLicenseClient = new ValistLicenseClient(provider, 80001);
     expect(valistLicenseClient).toBeDefined();
   });
 });
